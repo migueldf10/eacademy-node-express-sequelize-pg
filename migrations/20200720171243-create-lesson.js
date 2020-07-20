d10f43b9c36549805d7aa020e1ed737a29db0657
migueldf10 <migueldf10@gmail.com>
2020-07-20 19:16:10 +0200

    created data models

---
+++ migrations/20200720171243-create-lesson.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('lessons', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      courseId: {
+        type: Sequelize.INTEGER
+      },
+      priority: {
+        type: Sequelize.INTEGER
+      },
+      videUrl: {
+        type: Sequelize.STRING
+      },
+      published: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('lessons');
+  }
+};
---
+++ models/lesson.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class lesson extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  lesson.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    courseId: DataTypes.INTEGER,
+    priority: DataTypes.INTEGER,
+    videUrl: DataTypes.STRING,
+    published: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'lesson',
+  });
+  return lesson;
+};
