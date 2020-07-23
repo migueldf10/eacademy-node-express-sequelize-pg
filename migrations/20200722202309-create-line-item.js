368f6954f31d1a3069df1a914a4b52f4e9bde63b
migueldf10 <migueldf10@gmail.com>
2020-07-23 12:30:12 +0200

    added transactions and Line Items

---
+++ migrations/20200722202309-create-line-item.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('lineItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "courses",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      price: {
+        type: Sequelize.FLOAT
+      },
+      transactionId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "transactions",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('lineItems');
+  }
+};
