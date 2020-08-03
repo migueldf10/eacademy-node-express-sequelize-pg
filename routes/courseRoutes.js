a1b5200bf0ea18c05356d32626f41f8a20c74789
migueldf10 <migueldf10@gmail.com>
2020-08-03 02:42:34 +0200

    Admins can modify courses

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Course = require('../models').course
 const Lesson = require('../models').lesson
 const Permission = require('../models').permission
+const jwtCheck = require('../middlewares/auth')
 
 
 router.get('/', async function (req, res, next) {
@@ -26,5 +27,24 @@ router.get('/:courseId', async function (req, res, next) {
 	}
 })
 
+router.put('/:courseId',jwtCheck, async function (req, res, next) {
+	const isAdmin = req.user['https://thedutchonlineacademy.com/roles'].includes('admin')
+	if(!isAdmin)return res.status(401).send('Not authorized')
+	const courseId = req.params.courseId
+	if(!courseId)return res.status(404).send('Not course')
+	const newCourse = req.body
+	try {
+		const selectedCourse = await Course.findByPk(courseId, { include: [Lesson], plain: true })
+		selectedCourse.title = newCourse.title || selectedCourse.title
+		selectedCourse.description = newCourse.description || selectedCourse.price
+		selectedCourse.price = newCourse.price || selectedCourse.description
+		selectedCourse.videoUrl = newCourse.videoUrl || selectedCourse.videoUrl
+
+		const savedCourse = await selectedCourse.save()
+		res.send(savedCourse)
+	} catch (e) {
+		console.log(e)
+	}
+})
 
 module.exports = router
